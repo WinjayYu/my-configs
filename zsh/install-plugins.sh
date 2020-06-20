9fc9043314411db9f25fc29c518e6cc1904f6f1c
WinjayYu <winjayyu@gmail.com>
2020-06-20 22:23:09 +0800

    Add git clone git-open

--- zsh/install-plugins.sh
+++ zsh/install-plugins.sh
@@ -6,7 +6,7 @@ sed -i "/^#/! s/plugins=.*/${zshplugins}/g" ~/.zshrc
 echo "Start clone plugins"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
 echo "Please run 'source ~/.zshrc', then enjoy coding"
 
 exit 0
